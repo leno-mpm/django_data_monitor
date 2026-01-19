b8960cbfd24574ecf38bf238feb6355d3dfdcb37
Gabriel Peláez <116526182+gabriel12305@users.noreply.github.com>
2026-01-19 18:49:17 +0000

    guia26

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 import requests
 from django.conf import settings
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 
 @login_required
+@permission_required('dashboard.index_viewer', raise_exception=True)
 def index(request):
     response = requests.get(settings.API_URL, timeout=10)
     posts = response.json()

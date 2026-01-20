75906aa638baf449e55056feac8842a5f099c2e4
Silvia Milena Pazmiño Montes <155207990+leno-mpm@users.noreply.github.com>
2026-01-20 04:54:46 +0000

    intento1

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -15,6 +15,7 @@ import os
 import pymysql
 
 pymysql.install_as_MySQLdb()
+pymysql.version_info = (2,2,1,"final", 0)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
